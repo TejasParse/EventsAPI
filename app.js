11edf76098a5a1dcd59159283c5eada43b3180a5
TejasParse <tejasparse1@gmail.com>
2022-06-10 00:51:06 +0530

    Error Handling Done

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const path = require("path");
 app.use(express.urlencoded({extended: true}))
 app.use("/api/v3/app", require("./routers/events"));
 
+app.use((err,req,res,next)=>{
+
+    return res.json({
+        status:"fail",
+        error: err.message
+    });
+
+});
+
+app.get("*",(req,res)=>{
+    return res.json({
+        status:"Path not found"
+    });
+});
 
 app.listen(3000,()=>{
     console.log("Listening to port 3000");
